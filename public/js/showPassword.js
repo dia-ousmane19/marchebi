7086dedbcb2d7708e9fdb3788a5f5ec168c7aeea
ousmane dia <ousmanedia846@gmail.com>
2020-06-15 19:51:37 +0000

    show password inscription

--- public/js/showPassword.js
+++ public/js/showPassword.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  // show password connexion
   $('#iconShowPassword').on('click',function(e){
     e.preventDefault();
     var passwordValue=$('#inputPassword').val();
@@ -13,5 +14,22 @@ $(document).ready(function() {
     }
 
   });
+  // fin
+  // show password inscription
+  $('#iconShowPasswordInscription').on('click',function(e){
+    e.preventDefault();
+    var passwordValue=$('#registration_form_Password').val();
+    var passwordType=$('#registration_form_Password').attr('type');
+    if (passwordType == 'password') {
+      $('#registration_form_Password').attr('type','text');
+      $(this).html('<i class="fas fa-eye"></i>')
+
+    }else {
+      $('#registration_form_Password').attr('type','password');
+      $(this).html('<i class="fas fa-eye-slash"></i>')
+    }
+
+  });
+
 
 });
